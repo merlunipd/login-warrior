a8fb77bc0597b4049f1f41ad22d49a6b1e0ca849
mamprinmarco <mamprinmarco@gmail.com>
2022-02-10 19:40:27 +0100

    test scatter con data

---
+++ src/PoC/Scatterplot.js
@@ -0,0 +1,40 @@
+var width = 1000, height = 600, spacing = 120;
+var parseDate = d3.timeParse("%Y-%m-%dT%H:%M:%S");
+
+var dati = [
+    { "utente": 191, "data": "2021-06-04 12:48:16.000", "tipoEvento": 3, "applicazione": "ERM", "ip": "93.66.96.236"},
+    { "utente": 191, "data": "2021-04-30 08:16:04.000", "tipoEvento": 2, "applicazione": "ERM", "ip": "93.66.97.82"},
+    { "utente": 191, "data": "2021-04-21 11:52:52.000", "tipoEvento": 3, "applicazione": "ERM", "ip": "2.35.120.104"},
+    { "utente": 191, "data": "2021-06-10 14:05:42.000", "tipoEvento": 1, "applicazione": "ERM", "ip": "93.66.96.236"}
+]
+
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + spacing / 2 + "," + spacing / 2 + ")");
+
+var xScale = d3.scaleTime()
+    .domain([new Date("2021-04-20 02:00"), new Date("2021-06-30 02:00")])
+    .range([0, width - spacing]);
+
+var yScale = d3.scaleLinear()
+    .domain([0, 3])
+    .range([height - spacing, 0]);
+
+var xAxis = d3.axisBottom(xScale);
+var yAxis = d3.axisLeft(yScale);
+
+svg.append("g").attr("transform", "translate(0," + (height - spacing) + ")").call(xAxis);
+svg.append("g").call(yAxis);
+
+var dots = svg.append("g")
+    .selectAll("dot")
+    .data(dati);
+
+dots.enter().append("circle")
+    .attr("cx", function (d) { return xScale(new Date(d.data)); })
+    .attr("cy", function (d) { return yScale(d.tipoEvento); })
+    .attr("r", 5)
+    .style("fill", "red");
