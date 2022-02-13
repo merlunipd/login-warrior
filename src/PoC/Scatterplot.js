34f8901315fed87e15b765cd60b5383dc69287e3
mamprinmarco <mamprinmarco@gmail.com>
2022-02-13 20:30:54 +0100

    Test con altre dimensioni

--- src/PoC/Scatterplot.js
+++ src/PoC/Scatterplot.js
@@ -14,6 +14,111 @@ var dati = [
     { "utente": 8934, "data": "2021-03-04 09:43:35.000", "tipoEvento": 3, "applicazione": "HRW", "ip": "" },
 ]
 
+//Test x: giorno y: ora  colore: tipologia  testo: ip+utente 
+
+//creazione spazio di disegno nel body
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + spacing / 2 + "," + spacing / 2 + ")");
+
+//creazione scale di misura
+var xScale = d3.scaleTime()
+    .domain([d3.min(dati, function (d) { return new Date(d.data); }), d3.max(dati, function (d) { return new Date(d.data); })])
+    .range([0, width - spacing]);
+
+var yScale = d3.scaleLinear()
+    .domain([0,24])
+    .range([height - spacing, 0]);
+/*
+var yScale = d3.scalePoint()
+    .domain(["login", "logout", "errore"])
+    .range([height - spacing, 0]);
+
+var yScale = d3.scaleLinear()
+    .domain([0, 3])
+    .range([height - spacing, 0]);
+*/
+
+//creazione assi e disegno degli assi
+var xAxis = d3.axisBottom(xScale);
+var yAxis = d3.axisLeft(yScale);
+
+svg.append("g").attr("transform", "translate(0," + (height - spacing) + ")").call(xAxis);
+svg.append("g").call(yAxis);
+
+//selezione e creazione dei punti
+var dots = svg.append("g")
+    .selectAll("dot")
+    .data(dati);
+
+//disegno dei pallini
+
+dots.enter().append("circle")
+    .attr("cx", function (d) { return xScale(new Date(d.data)); })
+    .attr("cy", function (d) { return yScale( (new Date(d.data)).getHours() ); })
+        /*
+        switch (d.tipoEvento) {
+            case 1:
+                return yScale("login");
+                break;
+            case 2:
+                return yScale("errore");
+                break;
+            case 3:
+                return yScale("logout");
+                break;
+        }
+        */
+    
+    .attr("r", 5)
+    .style("fill", function (d) { 
+        
+        switch (d.tipoEvento) {
+            case 1:
+                return "green";
+                break;
+            case 2:
+                return "red";
+                break;
+            case 3:
+                return "grey";
+                break;
+        }
+        
+    });
+dots.enter().append("text")
+    .attr("x", function (d) { return xScale(new Date(d.data)); } )
+    .attr("y", function (d) { return yScale( (new Date(d.data)).getHours() ) - 10 ; } )
+    .attr("dy", ".35em")
+    .text(function(d) { return ( d.utente +" / "+d.ip ); });
+
+//Nome degli assi
+svg.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", -30)
+    .attr("x", 0)
+    .attr("dy", ".75em")
+    .text("Ora");
+
+svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("y", height -spacing + 10)
+    .attr("x", width - spacing + 30 )
+    .attr("dx", ".75em")
+    .text("Data");
+
+
+
+
+
+/* test x: data y: applicazione colore: tipoEvento
+
+
 //creazione spazio di disegno nel body
 var svg = d3.select("body")
     .append("svg")
@@ -39,7 +144,7 @@ var yScale = d3.scaleLinear()
     .domain([0, 3])
     .range([height - spacing, 0]);
 */
-
+/*
 //creazione assi e disegno degli assi
 var xAxis = d3.axisBottom(xScale);
 var yAxis = d3.axisLeft(yScale);
@@ -69,6 +174,7 @@ dots.enter().append("circle")
                 break;
         }
         */
+       /*
     })
     .attr("r", 5)
     .style("fill", function (d) { 
@@ -103,3 +209,4 @@ svg.append("text")
     .attr("x", width - spacing + 30 )
     .attr("dx", ".75em")
     .text("Data");
+*/
