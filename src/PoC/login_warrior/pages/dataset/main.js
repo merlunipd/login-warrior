19ba3a458862bd22d855521185ad070241bf4188
marko <moroku96@gmail.com>
2022-02-15 15:41:26 +0100

    Caricamento dataset: unificato button per caricamento file e dataset

--- src/PoC/login_warrior/pages/dataset/main.js
+++ src/PoC/login_warrior/pages/dataset/main.js
@@ -1,23 +1,19 @@
 import loadCsv from './modules/loadCsv.js';
-import drawScatterplot from './modules/scatterplot.js';
 
 const setupDOM = () => {
   const csvInput = document.querySelector('#csvInput');
-  const csvInputButton = document.querySelector('#csvInputButton');
   const datasetButton = document.querySelector('#datasetButton');
 
-  csvInput.addEventListener('change', () => {
-    datasetButton.disabled = csvInput.files[0] === undefined;
-  });
-
-  csvInputButton.addEventListener('click', () => {
+  datasetButton.addEventListener('click', async () => {
     csvInput.click();
   });
 
-  datasetButton.addEventListener('click', async () => {
+  csvInput.addEventListener('change', async () => {
     const file = csvInput.files[0];
-    // eslint-disable-next-line no-unused-vars
-    const data = await loadCsv(file);
+    if (file !== undefined) {
+      // eslint-disable-next-line no-unused-vars
+      const data = await loadCsv(file);
+    }
   });
 };
 
