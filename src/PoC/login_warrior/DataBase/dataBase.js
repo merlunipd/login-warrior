a4cc3499e5c70ec85169d8eedd58eb42cd84c8fc
Emanuele999-it <emanuele.pase@studenti.unipd.it>
2022-02-17 09:57:59 +0100

    Eliminazione file inutili + modifiche minori

--- src/PoC/login_warrior/DataBase/dataBase.js
+++ src/PoC/login_warrior/DataBase/dataBase.js
@@ -3,64 +3,52 @@
 
 let db = null
 
-let Extracteddata=null;
-
-const note = {
-  title: "Data",
-  text: variabile
-}
-
-
-
-function viewNotes() {
+function viewNotes(variabile) {
   const request = indexedDB.open("Login-Warrior");
-
   request.onsuccess = e => {
     const tx = db.transaction("Data", "readonly");
     const pNotes = tx.objectStore("Data");
     const request = pNotes.openCursor();
     request.onsuccess = e => {
+      let Extracteddata = null;
       const cursor = e.target.result
       if (cursor) {
-        alert(`Title: ${cursor.key} Text: ${cursor.value.text} `);
         Extracteddata=cursor.value.text;
+        alert("Iterazione elementi DB: "+Extracteddata);
         cursor.continue();
       }
       console.log(Extracteddata);
+      return Extracteddata;
     }
   }
-
   request.onerror = e => {
     alert("Errore nell'ertrazioene dei dati");
   }
-
-
 }
 
-function addNote() {
+function addNote(variabile) {
   const request = indexedDB.open("Login-Warrior");
-
   request.onupgradeneeded = e => {
     alert("Aggiornamento avvenuto con successo");
   }
-
   request.onsuccess = e => {
+    const note = {
+      title: "Data",
+      text: variabile
+    }
     db = e.target.result;
     const tx = db.transaction("Data", "readwrite");
     const pNotes = tx.objectStore("Data");
     pNotes.add(note);
     alert("Dati aggiunti con successo");
   }
-
   request.onerror = e => {
     alert("Errore nell'aggiunta dei dati");
   }
 }
 
 function createDB() {
-
   const request = indexedDB.open("Login-Warrior");
-
   request.onupgradeneeded = e => {
     db = e.target.result;
     const pNotes = db.createObjectStore("Data",{autoIncrement: true});
@@ -71,6 +59,6 @@ function createDB() {
     alert("DB creato");
   }
   request.onerror = e => {
-    alert(`Errore nell'apertura del DB`);
+    alert("Errore nell'apertura del DB");
   }
 }
