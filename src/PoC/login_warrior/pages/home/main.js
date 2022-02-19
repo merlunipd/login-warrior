656936200b8362e7d999598d45982ab5ecf5c531
marko <moroku96@gmail.com>
2022-02-19 18:04:23 +0100

    Aggiunta gestione della sessione: salvataggio e caricamento

--- src/PoC/login_warrior/pages/home/main.js
+++ src/PoC/login_warrior/pages/home/main.js
@@ -1,11 +1,14 @@
 import loadDataset from './modules/loadDataset.js';
 import { createDB, deleteDB, readDB, updateDB } from '../../shared/modules/indexdDB.js';
+import { loadSession } from '../../shared/modules/session.js';
 
 const datasetInput = document.querySelector('#datasetInput');
 const datasetButton = document.querySelector('#datasetButton');
+const loadSessionButton = document.querySelector('#load-session-button');
+const loadSessionInput = document.querySelector('#load-session-input');
 const plotList = document.querySelector("#plot-list");
 
-datasetButton.addEventListener('click', async () => {
+datasetButton.addEventListener('click', () => {
   datasetInput.click();
 });
 
@@ -14,12 +17,36 @@ datasetInput.addEventListener('change', async () => {
   if (file !== undefined) {
     const data = await loadDataset(file);
 
+    const session = {
+      data: data,
+      configuration: undefined
+    };
+
     await deleteDB();
     await createDB();
-    await updateDB(data);
+    await updateDB(session);
 
     plotList.style.display = "block";
   } else {
     plotList.style.display = "none";
+  }
+});
+
+loadSessionButton.addEventListener("click", () => {
+  loadSessionInput.click();
+});
+
+loadSessionInput.addEventListener('change', async () => {
+  const file = loadSessionInput.files[0];
+  if (file !== undefined) {
+    const session = await loadSession(file);
+    
+    console.log(session);
+
+    await deleteDB();
+    await createDB();
+    await updateDB(session);
+
+    window.location.href = `../..${session.configuration.pathToPage}`;
   }
 });
--- src/PoC/login_warrior/pages/scatterplot_01/main.js
+++ src/PoC/login_warrior/pages/scatterplot_01/main.js
@@ -1,15 +1,44 @@
+// Importa moduli necessari
 import { drawScatterPlot, updateScatterPlot } from "./modules/scatterplot.js";
 import { readDB } from '../../shared/modules/indexdDB.js';
+import { saveSession } from '../../shared/modules/session.js';
 
-let data = await readDB();
 
-drawScatterPlot(data)
+// Carica sessione: dati e configurazione
+const session = await readDB();
+const data = session.data;
+let configuration = session.configuration;
 
+
+
+// Disegna il grafico
+drawScatterPlot(data);
+
+
+
+// Collegare il DOM con gli eventi
 document.querySelector("#filtro-evento").addEventListener("input", (event) => {
-  const filtroEvento = (event.target.value).toString(10);
-  if (filtroEvento) {
-    updateScatterPlot(data, data.filter(d => d.tipoEvento === filtroEvento));
+  configuration.filtroEvento = event.target.value;
+  if (configuration.filtroEvento) {
+    updateScatterPlot(data, data.filter(d => d.tipoEvento === configuration.filtroEvento));
   } else {
     updateScatterPlot(data, data);
   }
 });
+
+document.querySelector("#save-session-button").addEventListener("click", () => {
+  saveSession(data, configuration);
+});
+
+
+
+// Configurazione: ricarica o imposta nuova
+if (configuration) {
+  document.querySelector("#filtro-evento").value = configuration.filtroEvento;
+  document.querySelector("#filtro-evento").dispatchEvent(new Event('input'));
+} else {
+  configuration = {
+    pathToPage: window.location.pathname,
+    filtroEvento: ""
+  };
+}
