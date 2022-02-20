832ad35c5de670b3e2634284f5a12440e670e8c5
marko <moroku96@gmail.com>
2022-02-20 10:15:26 +0100

    Pulito codice scatterplot01 e aggiunte animazioni

--- src/PoC/login_warrior/pages/scatterplot_01/modules/scatterplot.js
+++ src/PoC/login_warrior/pages/scatterplot_01/modules/scatterplot.js
@@ -1,13 +1,20 @@
+// Varuabili
 const [width, height, spacing] = [1000, 600, 100];
+const circlesRadius = 5;
+const circlesRadiusGrowth = 6;
+const circlesOpacity = 0.4;
 
-const drawScatterPlot = (data) => {
+// Funzioni ausiliarie
+const createSvg = () => {
   const svg = d3.select("#scatterplot")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
     .append("g")
     .attr("transform", "translate(" + spacing / 2 + "," + spacing / 2 + ")");
+};
 
+const getScales = (data) => {
   const xScale = d3.scaleTime()
     .domain([
       d3.min(data, d => new Date(d.data)),
@@ -19,14 +26,18 @@ const drawScatterPlot = (data) => {
     .domain([0, 24])
     .range([height - spacing, 0]);
 
-  svg.append("g")
+  return [xScale, yScale];
+};
+
+const drawAxis = (xScale, yScale) => {
+  d3.select("svg g").append("g")
     .attr("transform", "translate(0," + (height - spacing) + ")")
     .call(d3.axisBottom(xScale));
 
-  svg.append("g")
+  d3.select("svg g").append("g")
     .call(d3.axisLeft(yScale));
 
-  svg.append("text")
+  d3.select("svg g").append("text")
     .attr("class", "y label")
     .attr("text-anchor", "end")
     .attr("y", -30)
@@ -34,22 +45,24 @@ const drawScatterPlot = (data) => {
     .attr("dy", ".75em")
     .text("Ora");
 
-  svg.append("text")
+  d3.select("svg g").append("text")
     .attr("class", "x label")
     .attr("text-anchor", "end")
     .attr("y", height - spacing + 10)
     .attr("x", width - spacing + 30)
     .attr("dx", ".75em")
     .text("Data");
+};
 
+const drawCircles = (data, xScale, yScale) => {
   d3.select("svg g").selectAll("circle")
     .data(data, d => d.id)
     .enter()
     .append("circle")
     .attr("cx", function (d) { return xScale(new Date(d.data)); })
     .attr("cy", function (d) { return yScale((new Date(d.data)).getHours() + (new Date(d.data)).getMinutes() / 60); })
-    .attr("r", 5)
-    .style("opacity", 0.5)
+    .attr("r", circlesRadius)
+    .style("opacity", circlesOpacity)
     .style("fill", function (d) {
       switch (d.tipoEvento) {
         case "1":
@@ -59,54 +72,44 @@ const drawScatterPlot = (data) => {
         case "3":
           return "grey";
       }
+    })
+    .on('mouseover', function () {
+      d3.select(this)
+        .transition()
+        .duration('50')
+        .attr("r", circlesRadius + circlesRadiusGrowth)
+        .style("opacity", 1)
+        .style("stroke", "black");
+    })
+    .on('mouseout', function () {
+      d3.select(this).transition()
+        .duration('500')
+        .attr("r", circlesRadius)
+        .style("opacity", circlesOpacity)
+        .style("stroke", "none");
     });
 
   d3.select("svg g").selectAll("circle")
     .data(data, d => d.id)
     .append("title")
     .text(d => `ID: ${d.id}\nIP: ${d.ip}\nUTENTE: ${d.utente}\nTIPO EVENTO: ${d.tipoEvento}\nDATA: ${d.data}\nAPPLICAZIONE: ${d.applicazione}`);
+
+  d3.select("svg g").selectAll("circle")
+    .data(data, d => d.id)
+    .exit()
+    .remove();
+};
+
+const drawScatterPlot = (data) => {
+  createSvg();
+  const [xScale, yScale] = getScales(data);
+  drawAxis(xScale, yScale);
+  drawCircles(data, xScale, yScale);
 };
 
 const updateScatterPlot = (dataInitial, dataUpdated) => {
-  const xScale = d3.scaleTime()
-    .domain([
-      d3.min(dataInitial, d => new Date(d.data)),
-      d3.max(dataInitial, d => new Date(d.data))
-    ])
-    .range([0, width - spacing]);
-
-  const yScale = d3.scaleLinear()
-    .domain([0, 24])
-    .range([height - spacing, 0]);
-
-  d3.select("svg g").selectAll("circle")
-    .data(dataUpdated, d => d.id)
-    .enter()
-    .append("circle")
-    .attr("cx", function (d) { return xScale(new Date(d.data)); })
-    .attr("cy", function (d) { return yScale((new Date(d.data)).getHours() + (new Date(d.data)).getMinutes() / 60); })
-    .attr("r", 5)
-    .style("opacity", 0.5)
-    .style("fill", function (d) {
-      switch (d.tipoEvento) {
-        case "1":
-          return "green";
-        case "2":
-          return "red";
-        case "3":
-          return "grey";
-      }
-    });
-
-  d3.select("svg g").selectAll("circle")
-    .data(dataUpdated, d => d.id)
-    .append("title")
-    .text(d => `ID: ${d.id}\nIP: ${d.ip}\nUTENTE: ${d.utente}\nTIPO EVENTO: ${d.tipoEvento}\nDATA: ${d.data}\nAPPLICAZIONE: ${d.applicazione}`);
-
-  d3.select("svg g").selectAll("circle")
-    .data(dataUpdated, d => d.id)
-    .exit()
-    .remove();
+  const [xScale, yScale] = getScales(dataInitial);
+  drawCircles(dataUpdated, xScale, yScale)
 };
 
 export { drawScatterPlot, updateScatterPlot };
