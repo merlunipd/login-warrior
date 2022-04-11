f3a96152f8f6117db239fd7ba5e592f0a2c3ddd7
marko <moroku96@gmail.com>
2022-04-11 14:59:50 +0200

    aggiunta interfaccia Model

---
+++ src/login_warrior/src/model/Model.js
@@ -0,0 +1,9 @@
+/**
+ * @interface
+ * Interfaccia per il modello. Utile per avere un tipo di supporto per astrarre le classi principali del modello.
+ */
+class Model {
+  constructor() {
+    throw new Error("Interfaces can not be instantiated");
+  }
+}
