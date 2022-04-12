72fb2141ca00ede29dc9774d62f2c932adac00bc
moroku <moroku96@gmail.com>
2022-04-12 15:29:57 +0200

    completato primo draft classe Dataset

--- src/login_warrior/src/model/Dataset.js
+++ src/login_warrior/src/model/Dataset.js
@@ -31,7 +31,6 @@ export default class Dataset {
     this.#filters = filters;
   }
 
-  // TODO
   /**
    * Ritorna i punti del dataset, filtrati e poi (se necessario) campionati
    * @param {number} samplesLimit Numero massimo di punti del dataset da campionare
@@ -41,11 +40,22 @@ export default class Dataset {
     return this.#sampleDataset(this.#filterDataset(), samplesLimit);
   }
 
-  // TODO
-  getDatasetUnfiltered() { }
+  /**
+   * Ritorna i punti del dataset, (se necessario) campionati ma non filtrati
+   * @param {number} samplesLimit Numero massimo di punti del dataset da campionare
+   * @returns {DataPoint[]} Punti del dataset
+   */
+  getDatasetUnfiltered(samplesLimit) { 
+    return this.#sampleDataset(this.#dataset, samplesLimit);
+  }
 
-  // TODO
-  getDatasetUnsampled() { }
+  /**
+   * Ritorna i punti del dataset, filtrati ma non campionati
+   * @returns {DataPoint[]} Punti del dataset
+   */
+  getDatasetUnsampled() {
+    return this.#filterDataset();
+  }
 
   /**
    * Ritorna tutti i punti del dataset, senza filtri e senza campionamento
@@ -69,21 +79,73 @@ export default class Dataset {
     this.#filters = filters;
   }
 
-  // TODO
+  /**
+   * Funzione per filtrare il dataset corrente utilizzando i filtri correnti.
+   * 
+   * Attenzione: il filtro della data viene eseguito considerando
+   * l'uguaglianza di giorno, mese, anno.
+   * @returns {DataPoint[]} Dataset filtrato
+   */
   #filterDataset() {
-
+    return this.#dataset.filter(dataPoint => {
+      const idFilter = this.#filters.getId();
+      if (idFilter === null) {
+        return true;
+      } else {
+        return dataPoint.getId() === idFilter;
+      }
+    })
+      .filter(dataPoint => {
+        const ipFilter = this.#filters.getIp();
+        if (ipFilter === null) {
+          return true;
+        } else {
+          return dataPoint.getIp() === ipFilter;
+        }
+      })
+      .filter(dataPoint => {
+        const dateFilter = this.#filters.getDate();
+        if (dateFilter === null) {
+          return true;
+        } else {
+          return dataPoint.getDate().getFullYear() === dateFilter.getFullYear() &&
+          dataPoint.getDate().getMonth() === dateFilter.getMonth() &&
+          dataPoint.getDate().getDate() === dateFilter.getDate();
+        }
+      })
+      .filter(dataPoint => {
+        const eventFilter = this.#filters.getEvent();
+        if (eventFilter === null) {
+          return true;
+        } else {
+          return dataPoint.getEvent() === eventFilter;
+        }
+      })
+      .filter(dataPoint => {
+        const applicationFilter = this.#filters.getApplication();
+        if (applicationFilter === null) {
+          return true;
+        } else {
+          return dataPoint.getApplication() === applicationFilter;
+        }
+      })
   }
 
   /**
-   * Funzione di campionamento del dataset (casuale per il momento)
+   * Funzione di campionamento del dataset. 
+   * Se il numero di punti del dataset è minore o uguale di samplesLimit, ritorna una copia del dataset stesso, altrimenti effettua un campionamento casuale dei dati.
    * @param {DataPoint[]} dataset Dataset da campionare
    * @param {number} samplesLimit Numero massimo di punti che ritorna la funzione di campionamento
    * @returns {DataPoint[]} Dataset campionato 
    */
   #sampleDataset(dataset, samplesLimit) {
-    const datasetCopy = [...dataset];
-    const shuffledDataset = datasetCopy.sort(() => 0.5 - Math.random());
-    const sampledDataset = shuffledDataset.slice(0, samplesLimit);
-    return sampledDataset;
+    if (dataset.length <= samplesLimit) {
+      return [...dataset];
+    } else {
+      const datasetCopy = [...dataset];
+      const shuffledDataset = datasetCopy.sort(() => 0.5 - Math.random());
+      const sampledDataset = shuffledDataset.slice(0, samplesLimit);
+      return sampledDataset;
+    }
   }
 }
