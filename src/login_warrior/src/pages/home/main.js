569ffe868d6ec214b60ee0026d11fc081b3035fe
marko <moroku96@gmail.com>
2022-04-17 16:12:05 +0200

    Aggiunte alcune pagine

---
+++ src/login_warrior/src/pages/home/main.js
@@ -0,0 +1,5 @@
+// TODO: decommentare quando tutte le classi saranno funzionanti
+/*
+import HomeController from "../../controller/HomeController.js";
+const homeController = new HomeController();
+*/
