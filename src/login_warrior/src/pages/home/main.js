ef0f51aa938915979d124ae51017dd5e41d2c639
marko <moroku96@gmail.com>
2022-04-21 10:38:26 +0200

    fix firefox

--- src/login_warrior/src/pages/home/main.js
+++ src/login_warrior/src/pages/home/main.js
@@ -1,2 +1,9 @@
 import HomeController from "../../controller/HomeController.js";
 const homeController = new HomeController();
+
+/*
+ Firefox: quando si naviga tra le pagine con i comandi del browser,
+ non viene rieseguito il codice JS. Questa riga serve per forzare
+ la ri-esecuzione.
+*/ 
+window.onunload = function(){}; 
--- src/login_warrior/src/pages/shared/main_visualizations.js
+++ src/login_warrior/src/pages/shared/main_visualizations.js
@@ -1,2 +1,9 @@
 import VisualizationsController from "../../controller/VisualizationsController.js";
 const visualizationsController = new VisualizationsController();
+
+/*
+ Firefox: quando si naviga tra le pagine con i comandi del browser,
+ non viene rieseguito il codice JS. Questa riga serve per forzare
+ la ri-esecuzione.
+*/ 
+window.onunload = function(){}; 
