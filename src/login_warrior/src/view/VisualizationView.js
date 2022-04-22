98360492a1c3987e7b56b95f79b32578d11a1904
riccardo-contin <ricky11.contin@gmail.com>
2022-04-22 15:12:21 +0200

    Aggiunto drawer Scatter Plot 2

--- src/login_warrior/src/view/VisualizationView.js
+++ src/login_warrior/src/view/VisualizationView.js
@@ -1,4 +1,5 @@
 import ScatterPlot1 from './drawers/ScatterPlot1.js';
+import ScatterPlot2 from './drawers/ScatterPlot2.js';
 
 import FilterId from './filters/FilterId.js';
 import FilterIp from './filters/FilterIp.js';
@@ -41,7 +42,9 @@ export default class VisualizationView {
       case 1:
         this.visualization = new Visualization('#visualization', null, new ScatterPlot1());
         break;
-
+      case 2:
+        this.visualization = new Visualization('#visualization', null, new ScatterPlot2());
+        break;
       default:
         break;
     }
